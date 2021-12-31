85bd536588d5ca2fcee826c25837ad24de8009c7
lindba <ora2dba@gmail.com>
2021-12-31 23:39:30 +0530

    Update puipVnic.sh

--- puipVnic.sh
+++ puipVnic.sh
@@ -9,13 +9,14 @@ instIp=$(ip addr show dev enp0s3 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f3);
 nscMac=$(ip link show dev $instIfc | grep ether | tr -s ' ' | cut -d' ' -f3);
 cat > ns_$ns.cfg <<!
 ip=$puip
-pfx=24
+pfx=24 
 gw=$gw
 nscMac=$nscMac
 nsh=nsh$ns
 nsc=ns$ns
 brns=brns
 instIfc=$instIfc
+instIp=$instIp
 !
 curl -O https://raw.githubusercontent.com/lindba/scripts/master/ns/ns.sh; chmod +x ns.sh;
 curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.service; sed -i -e s/hostname/$(hostname)/g puipVnic.service; mv puipVnic.service /lib/systemd/system/; systemctl enable puipVnic;
@@ -24,4 +25,3 @@ curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.service;
 <<cloudInit
   curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.sh; chmod +x puipVnic.sh; ./puipVnic.sh
 cloudInit
-
