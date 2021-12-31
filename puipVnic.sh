e16d8c398a0ae3bc4a636c4dabd01a233dfda7a5
lindba <ora2dba@gmail.com>
2021-12-31 23:05:42 +0530

    Add files via upload

--- puipVnic.sh
+++ puipVnic.sh
@@ -3,8 +3,9 @@
 cd /opt/puipVnic;
 ns=$(hostname);
 puip=$(oci-public-ip -g) ; [[ $? == 1 ]] && puip=$(curl --insecure  https://ipinfo.io/ip);
-gw=$(echo 192.168.41.2 | cut -d. -f1,2,3).1;
-instIfc=$(grep  '^en' /proc/net/dev | head -1 | cut -d: -f1);
+gw=$(echo $puip | cut -d. -f1,2,3).1;
+#instIfc=$(grep  '^en' /proc/net/dev | head -1 | cut -d: -f1);
+instIp=$ip addr show dev enp0s3 | grep 'inet ' | tr -s ' ' | cut -d ' ' -f3);
 nscMac=$(ip link show dev $instIfc | grep ether | tr -s ' ' | cut -d' ' -f3);
 cat > ns_$ns.cfg <<!
 ip=$puip
@@ -12,11 +13,11 @@ pfx=24
 gw=$gw
 nscMac=$nscMac
 nsh=nsh$ns
-nsc=$instIfc
+nsc=ns$ns
 brns=brns
 instIfc=$instIfc
 !
-curl -O https://github.com/lindba/scripts/blob/master/ns/ns.sh; chmod +x ns.sh;
-curl -O https://github.com/lindba/puipVnic/blob/main/puipVnic.service; mv puipVnic.service /lib/systemd/system/; systemd enable puipVnic;
+curl -O https://raw.githubusercontent.com/lindba/ns/main/ns.sh; chmod +x ns.sh;
+curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.service; sed -i -e s/hostname/$(hostname)/g puipVnic.service; mv puipVnic.service /lib/systemd/system/; systemctl enable puipVnic;
 
 
