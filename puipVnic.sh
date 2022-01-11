3b1feb339e28f23c017dcea6c68017994b8b917a
lindba <ora2dba@gmail.com>
2022-01-11 23:22:29 +0530

    Update puipVnic.sh

--- puipVnic.sh
+++ puipVnic.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # /opt/puipVnic/puipVnic.sh
-systemctl stop firewalld; systemctl disable firewalld; 
+systemctl stop firewalld; systemctl disable firewalld;
 mkdir /opt/puipVnic; cd /opt/puipVnic;
 ns=$(hostname);
 puip=$(oci-public-ip -g) ; [[ $? == 1 ]] && puip=$(curl --insecure  https://ipinfo.io/ip);
 gw=$(echo $puip | cut -d. -f1,2,3).1;
-instIfc=$(grep  'en' /proc/net/dev | head -1 | cut -d: -f1);
+instIfc=$(grep  'en' /proc/net/dev | head -1 | cut -d: -f1|sed -e "s/ //g");
+#instIp=$(ip addr show | grep 'inet ' | tr -s ' ' | cut -d ' ' -f3);
 instIp=$(ip addr show | grep 'inet ' | tr -s ' ' | cut -d ' ' -f3 | grep -v 127|head -1);
 nscMac=$(ip link show dev $instIfc | grep ether | tr -s ' ' | cut -d' ' -f3);
 cat > ns_$ns.cfg <<!
-ip=$puip
+ip=$(echo $puip|sed -e "s/ //g")
 pfx=24
 gw=$gw
 nscMac=$nscMac
@@ -22,10 +23,3 @@ instIp=$instIp
 !
 curl -O https://raw.githubusercontent.com/lindba/scripts/master/ns/ns.sh; chmod +x ns.sh;
 curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.service; sed -i -e s/hostname/$(hostname)/g puipVnic.service; mv puipVnic.service /lib/systemd/system/; for act in start enable; do systemctl $act puipVnic; done;
-
-
-<<cloudInit
-  curl -O https://raw.githubusercontent.com/lindba/puipVnic/main/puipVnic.sh; chmod +x puipVnic.sh; sudo ./puipVnic.sh
-cloudInit
-
-
